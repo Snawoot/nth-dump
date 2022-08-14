7a96dac351ff6bc9d2f0f1a4e83bcb6ef6cc0c2c
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2022-08-15 00:41:53 +0300

    fix

--- cmd/nth-dump/opts_unix.go
+++ cmd/nth-dump/opts_unix.go
@@ -2,6 +2,10 @@
 
 package main
 
+import (
+	"flag"
+)
+
 var (
 	noqr        = flag.Bool("noqr", false, "do not print QR code with URL")
 	nowait      = flag.Bool("nowait", true, "do not wait for key press after output")
--- cmd/nth-dump/opts_windows.go
+++ cmd/nth-dump/opts_windows.go
@@ -2,6 +2,10 @@
 
 package main
 
+import (
+	"flag"
+)
+
 var (
 	noqr        = flag.Bool("noqr", true, "do not print QR code with URL")
 	nowait      = flag.Bool("nowait", false, "do not wait for key press after output")
