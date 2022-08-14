9dfb366b32fa7621b93962a620d3d591e699781e
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2022-08-14 19:24:05 +0300

    WIP: config retrieval

--- cmd/nth-dump/main.go
+++ cmd/nth-dump/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/Snawoot/nth-dump/nthclient"
 )
@@ -14,6 +16,7 @@ var version = "undefined"
 var (
 	// global options
 	showVersion = flag.Bool("version", false, "show program version and exit")
+	timeout     = flag.Duration("timeout", 10*time.Second, "operation timeout")
 )
 
 func run() int {
@@ -22,7 +25,16 @@ func run() int {
 		fmt.Println(version)
 		return 0
 	}
-	log.Println(nthclient.CalculateAPIHostname(nthclient.DefaultSettings.DomainSeed, nthclient.DefaultSettings.TLD))
+
+	ctx, cl := context.WithTimeout(context.Background(), *timeout)
+	defer cl()
+
+	nc := nthclient.New()
+	_, err := nc.GetServerConfig(ctx)
+	if err != nil {
+		log.Fatalf("can't get server config: %v", err)
+	}
+
 	return 0
 }
 
--- nthclient/client.go
+++ nthclient/client.go
@@ -1,6 +1,12 @@
 package nthclient
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+)
 
 // Client for nthLink API backend, capable of retrieving server configuration from it.
 type Client struct {
@@ -34,4 +40,32 @@ func (c *Client) WithSettings(settings *Settings) *Client {
 	newClient := c.Clone()
 	newClient.settings = settings
 	return newClient
+}
+
+func (c *Client) prepareRequest(ctx context.Context) (*http.Request, error) {
+	url := url.URL{
+		Scheme: "https",
+		Host:   CalculateAPIHostname(c.settings.DomainSeed, c.settings.TLD),
+		Path:   ConfigRoutePath,
+		RawQuery: url.Values{
+			"key":        []string{c.settings.PlatformKey},
+			"id":         []string{c.settings.ID},
+			"lang":       []string{c.settings.Language},
+			"appVersion": []string{c.settings.AppVersion},
+		}.Encode(),
+	}
+	log.Printf("url = %q", url.String())
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("unable to construct request: %w", err)
+	}
+	return req, nil
+}
+
+func (c *Client) GetServerConfig(ctx context.Context) ([]byte, error) {
+	_, err := c.prepareRequest(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("unable to prepare request: %w", err)
+	}
+	return nil, nil
 }
