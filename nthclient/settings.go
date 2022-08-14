bd768dc7a811084c61a0cc67e0325d3b5bb83af8
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2022-08-14 18:57:22 +0300

    add UUID ID

--- nthclient/settings.go
+++ nthclient/settings.go
@@ -1,11 +1,15 @@
 package nthclient
 
+import "github.com/google/uuid"
+
 // Settings define important constants required for Client operation
 type Settings struct {
 	DomainSeed  string
 	PlatformKey string
 	JSONSeed    string
 	TLD         string
+	Language    string
+	ID          string
 }
 
 // DefaultSettings is Settings with working defaults
@@ -14,4 +18,6 @@ var DefaultSettings = &Settings{
 	PlatformKey: "jk8Gh9wweC4gF8et",
 	JSONSeed:    "Gu82kdDgus0248gzkqpsl948ab7a8dse",
 	TLD:         "info",
+	Language:    "en-US",
+	ID:          uuid.Must(uuid.NewRandom()).String(),
 }
